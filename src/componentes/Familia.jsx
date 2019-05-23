452e44d9a63c13fb895642edca5fa61c6fdcdb83
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2019-05-22 21:36:22 -0300

    Corrigidos erros de digitação na documentação

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -4,11 +4,11 @@ import { filhosComProps } from '../utils/ultils'
 export default props =>
     <div>
         <h1>Família</h1>
-         {React.Children.map(props.children, filho => {
+         {/* {React.Children.map(props.children, filho => {
             return React.cloneElement(filho, { ...props })})
 
-        } 
-        {/* {filhosComProps(props)} */ }
+        } **  */}
+        {{filhosComProps(props)}  }
         {/* {React.cloneElement(props.children, { ...props })} } */}
         {/* {React.cloneElement(props.children,{sobrenome : props.sobrenome} )} */ }
         {/* {props.children} */ }
